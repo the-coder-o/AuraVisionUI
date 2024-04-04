89ca0bc6e2534517b31363666cab9a5922ff8015
Abdul basit <115723431+the-coder-o@users.noreply.github.com>
2024-04-04 16:49:49 +0500

    optimized file structure

---
+++ app/documentation/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Docs = () => {
+  return <div>Coming Soon</div>;
+};
+
+export default Docs;
---
+++ app/templates/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Templates = () => {
+  return <div>Coming Soon</div>;
+};
+
+export default Templates;
